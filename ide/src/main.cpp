5609762b7753d41fb600f421c989374bff74a93f
Thorben Hasenpusch <t.hasenpusch@icloud.com>
2019-11-11 20:36:49 +0100

    Simplify the IDE setup.

--- ide/src/main.cpp
+++ ide/src/main.cpp
@@ -1,11 +1,39 @@
-#include "mainwindow.hpp"
+/*
+ * Copyright 2017-2019 Thorben Hasenpusch <t.hasenpusch@icloud.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #include <QApplication>
+#include <QMainWindow>
+#include <QTextEdit>
+
+static QMainWindow *createMainWindow();
+static QTextEdit *createTextEdit(QWidget *parent);
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    QMainWindow *mainWindow = createMainWindow();
+    mainWindow->setCentralWidget(createTextEdit(mainWindow));
+
+    mainWindow->show();
+
+    return app.exec();
+}
+
+QMainWindow *createMainWindow()
+{
+    QMainWindow *mainWindow = new QMainWindow();
+    mainWindow->resize(800, 600);
+    mainWindow->setWindowTitle("Duality IDE");
+    return mainWindow;
+}
+
+QTextEdit *createTextEdit(QWidget *parent)
+{
+    QTextEdit *textEdit = new QTextEdit(parent);
+    textEdit->setAcceptRichText(false);
+    return textEdit;
 }
