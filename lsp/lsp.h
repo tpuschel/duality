58aa8fc261f48433ddaf21c45a83ed4ce1cff269
Thorben Hasenpusch <t.hasenpusch@icloud.com>
2020-07-17 19:27:18 +0200

    Add missing initializers.

--- lsp/lsp.h
+++ lsp/lsp.h
@@ -823,7 +823,9 @@ void process_document(struct dy_lsp_ctx *ctx, struct document *doc)
 
     struct dy_core_ctx core_ctx = {
         .running_id = ast_to_core_ctx.running_id,
-        .bound_constraints = dy_array_create(sizeof(struct dy_bound_constraint), DY_ALIGNOF(struct dy_bound_constraint), 1)
+        .bound_constraints = dy_array_create(sizeof(struct dy_bound_constraint), DY_ALIGNOF(struct dy_bound_constraint), 1),
+        .subtype_assumption_cache = dy_array_create(sizeof(struct dy_subtype_assumption), DY_ALIGNOF(struct dy_subtype_assumption), 64),
+        .supertype_assumption_cache = dy_array_create(sizeof(struct dy_subtype_assumption), DY_ALIGNOF(struct dy_subtype_assumption), 64)
     };
 
     struct dy_constraint c;
