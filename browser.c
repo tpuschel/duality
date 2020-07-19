1f6dad89fdfd2d9b731278ce3b2098a50b676d1b
Thorben Hasenpusch <t.hasenpusch@icloud.com>
2020-07-19 21:01:46 +0200

    Add translation unit specifically for use with emscripten.

---
+++ browser.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2020 Thorben Hasenpusch <t.hasenpusch@icloud.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define _GNU_SOURCE // For glibc's asprintf.
+
+#include "syntax/parser.h"
+#include "syntax/ast_to_core.h"
+
+#include "core/check.h"
+#include "core/eval.h"
+
+#include <string.h>
+
+/**
+ * This file declares and implements the functions used
+ * by the browser version of Duality via emscripten.
+ */
+
+const char *process_code(const char *text, size_t text_length_in_bytes);
+
+static inline struct dy_stream stream_from_string(dy_string_t s);
+static inline void null_stream(dy_array_t *buffer, void *env);
+
+const char *process_code(const char *text, size_t text_length_in_bytes)
+{
+    dy_string_t s = {
+        .ptr = text,
+        .size = text_length_in_bytes
+    };
+
+    struct dy_parser_ctx parser_ctx = {
+        .stream = stream_from_string(s),
+        .string_arrays = dy_array_create(sizeof(dy_array_t *), DY_ALIGNOF(dy_array_t *), 32)
+    };
+
+    struct dy_ast_do_block program_ast;
+    if (!dy_parse_file(&parser_ctx, &program_ast)) {
+        return "Failed to parse program.\n";
+    }
+
+    struct dy_ast_to_core_ctx ast_to_core_ctx = {
+        .running_id = 0,
+        .bound_vars = dy_array_create(sizeof(struct dy_ast_to_core_bound_var), DY_ALIGNOF(struct dy_ast_to_core_bound_var), 64)
+    };
+
+    struct dy_core_expr program = dy_ast_do_block_to_core(&ast_to_core_ctx, program_ast);
+
+    dy_ast_do_block_release(program_ast.body);
+
+    struct dy_core_ctx core_ctx = {
+        .running_id = ast_to_core_ctx.running_id,
+        .bound_constraints = dy_array_create(sizeof(struct dy_bound_constraint), DY_ALIGNOF(struct dy_bound_constraint), 64),
+        .subtype_assumption_cache = dy_array_create(sizeof(struct dy_subtype_assumption), DY_ALIGNOF(struct dy_subtype_assumption), 64),
+        .supertype_assumption_cache = dy_array_create(sizeof(struct dy_subtype_assumption), DY_ALIGNOF(struct dy_subtype_assumption), 64)
+    };
+
+    struct dy_constraint constraint;
+    bool have_constraint = false;
+    struct dy_core_expr checked_program = dy_check_expr(&core_ctx, program, &constraint, &have_constraint);
+    assert(!have_constraint);
+
+    bool is_value = false;
+    struct dy_core_expr result = dy_eval_expr(&core_ctx, checked_program, &is_value);
+
+    dy_array_t stringified_expr = dy_array_create(sizeof(char), DY_ALIGNOF(char), 64);
+    dy_core_expr_to_string(result, &stringified_expr);
+
+    dy_array_add(&stringified_expr, &(char){ '\0' });
+
+    return stringified_expr.buffer;
+}
+
+struct dy_stream stream_from_string(dy_string_t s)
+{
+    dy_array_t buffer = dy_array_create(sizeof(char), DY_ALIGNOF(char), s.size);
+    for (size_t i = 0; i < s.size; ++i) {
+        dy_array_add(&buffer, s.ptr + i);
+    }
+
+    return (struct dy_stream){
+        .get_chars = null_stream,
+        .buffer = buffer,
+        .env = NULL,
+        .current_index = 0
+    };
+}
+
+void null_stream(dy_array_t *buffer, void *env)
+{
+    (void)buffer;
+    (void)env;
+    return;
+}
